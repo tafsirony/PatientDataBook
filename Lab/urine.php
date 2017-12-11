437bca536e765cdf1dce8f9d48878d5281a7d39d
ruhul0 <ruhul20@gmail.com>
2017-12-11 17:21:49 +0600

    URINE & WBC test

---
+++ Lab/urine.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="1.php" method="get">
+COLOR: <input type="text" name="WBC"><br>
+APPEARANCE: <input type="text" name="RBC"><br>
+GLUCOSE: <input type="text" name="HB"><br>
+BILIRUBIN: <input type="text" name="HCT"><br>
+SPECGRAV: <input type="text" name="MCV"><br>
+PH: <input type="text" name="MCH"><br>
+<input type="submit">
+</form>
+<?php
+$COLORRef="YELLOW";
+$APPEARANCERef="CLEAR";
+$GLUCOSERef="NEGATIVE";
+$BILIRUBINRef="NEGATIVE";
+$SPECGRAVRef="1.003-1.035";
+$PHRef="NEGATIVE";
+?>
+</body>
+</html>
